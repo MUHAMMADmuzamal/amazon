95d8d8b67c68dca0685280f210ae86fc2a5f8c09
MuhammadMuzamal <muhammadmuzamal42@gmail.com>
2021-03-12 03:37:30 +0500

    second

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './components/Login/Login'
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import {BrowserRouter as Router,Switch, Route} from  'react-router-dom';
+import Payment from "./components/Payment/Payment";
 function App() {
 
   const [{  }, dispatch] = useStateValue();
@@ -43,6 +44,10 @@ function App() {
         <Route path="/checkout">
           <Header/>
           <Checkout/>
+        </Route>
+        <Route path="/payment">
+          <Header/>
+          <Payment/>
         </Route>
         <Route path="/">
           <Header/>
